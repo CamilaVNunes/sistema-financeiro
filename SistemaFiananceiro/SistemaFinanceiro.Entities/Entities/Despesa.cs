a9082adcb2b9370ead4b6abd8404071dae6e7ea0
CamilaVNunes <cvn.camila@outlook.com>
2023-04-02 22:56:32 -0300

    entidades;

---
+++ SistemaFiananceiro/SistemaFinanceiro.Entities/Entities/Despesa.cs
@@ -0,0 +1,31 @@
+﻿using SistemaFinanceiro.Entities.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaFinanceiro.Entities.Entities
+{
+    public class Despesa
+    {
+        public decimal Valor { get; set; }
+        public int Mes { get; set; }
+        public int Ano { get; set; }
+
+        public EnumTipoDespesa TipoDespesa { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime DataAlteracao { get; set; }
+
+        public DateTime DataPagamento { get; set; }
+
+        public DateTime DataVencimento { get; set; }
+
+        public bool Pago { get; set; }
+
+        public bool DespesaAntrasada { get; set; }
+
+        [ForeignKey("Categoria")]
+        [Column(Order = 1)]
+        public int IdCategoria { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
---
+++ SistemaFiananceiro/SistemaFinanceiro.Entities/Notifications/Notify.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro.Entities.Notifications
+{
+    public class Notify
+    {
+        public Notify()
+        {
+            Notifies = new List<Notify>();
+        }
+
+        [NotMapped]
+        public string NameProperty { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public List<Notify> Notifies { get; set; }
+
+        public bool ValidyPropertyString(string value, string nameProperty)
+        {
+            if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifies.Add(new Notify
+                {
+                    NameProperty = nameProperty,
+                    Message = "Campo Obrigatório"
+                });
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidyPropertyInteger(int value, string nameProperty)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifies.Add(new Notify
+                {
+                    NameProperty = nameProperty,
+                    Message = "Campo Obrigatório"
+                });
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
