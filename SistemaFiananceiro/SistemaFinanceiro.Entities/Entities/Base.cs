9e664dbb544b137964ca0ab5c7f63f305f102c70
CamilaVNunes <cvn.camila@outlook.com>
2023-04-02 22:56:23 -0300

    criando entidades;

---
+++ SistemaFiananceiro/SistemaFinanceiro.Entities/Entities/Base.cs
@@ -0,0 +1,14 @@
+﻿using SistemaFinanceiro.Entities.Notifications;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaFinanceiro.Entities.Entities
+{
+    public class Base : Notify
+    {
+        [Display(Name="Código")]
+        public int Id{ get; set; }
+
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+    }
+}
