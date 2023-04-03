1bddcc3f7a1bacd0881ae2b942acab08ba8f44bb
CamilaVNunes <cvn.camila@outlook.com>
2023-04-03 19:05:06 -0300

    criando as cascas das interfaces;

---
+++ SistemaFiananceiro/SistemaFinanceiro.Domain/Interfaces/Generics/IGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro.Domain.Interfaces.Generics
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task Add(T Obeject);
+        Task Delete(T Obeject);
+        Task<T> GetEntityById(int Id);
+        Task Update(T Obeject);
+        Task<List<T>> GetAll();
+    }
+}
